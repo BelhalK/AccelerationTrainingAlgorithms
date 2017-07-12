ba36186d853fd770166c15666d9759148630dcd4
Belhal KARIMI <Belhal.karimi@gmail.com>
2017-07-12 15:37:09 +0200

    mala

---
+++ laplace/newkernel_vs_rwm_yield.R
@@ -0,0 +1,116 @@
+#library(rstan)
+setwd("/Users/karimimohammedbelhal/Desktop/variationalBayes/mcmc_R_isolate/Dir2")
+  source('compute_LL.R') 
+  source('func_aux.R') 
+  source('func_cov.R') 
+  source('func_distcond.R') 
+  source('func_FIM.R') 
+  source('func_ggplot2.R') 
+  source('func_plots.R') 
+  source('func_simulations.R') 
+  source('ggplot2_global.R') 
+  # source('KL.R') 
+  #source('vi.R') 
+  source('global.R')
+  source('main.R')
+  source('mcmc_main.R') 
+  source('main_estep.R')
+  source('main_estep_mcmc.R') 
+  source('main_estep_morekernels.R') 
+  source('main_initialiseMainAlgo.R') 
+  source('main_mstep.R') 
+  source('SaemixData.R')
+  source('plots_ggplot2.R') 
+  source('saemix-package.R') 
+  source('SaemixModel.R') 
+  source('SaemixRes.R') 
+  source('SaemixObject.R') 
+  source('zzz.R') 
+  source("mixtureFunctions.R")
+setwd("/Users/karimimohammedbelhal/Documents/GitHub/saem/new_kernel")
+source('newkernel_main.R')
+source('main_estep_newkernel.R')
+
+
+require(ggplot2)
+require(gridExtra)
+require(reshape2)
+
+#####################################################################################
+# Theophylline
+
+# Data - changing gender to M/F
+# theo.saemix<-read.table("data/theo.saemix.tab",header=T,na=".")
+# theo.saemix$Sex<-ifelse(theo.saemix$Sex==1,"M","F")
+# saemix.data<-saemixData(name.data=theo.saemix,header=TRUE,sep=" ",na=NA, name.group=c("Id"),name.predictors=c("Dose","Time"),name.response=c("Concentration"),name.covariates=c("Weight","Sex"),units=list(x="hr",y="mg/L",covariates=c("kg","-")), name.X="Time")
+iter_mcmc = 50
+
+
+
+# Doc
+data(yield.saemix)
+saemix.data<-saemixData(name.data=yield.saemix,header=TRUE,name.group=c("site"),
+  name.predictors=c("dose"),name.response=c("yield"),
+  name.covariates=c("soil.nitrogen"),units=list(x="kg/ha",y="t/ha",
+  covariates=c("kg/ha")))
+
+yield.LP<-function(psi,id,xidep) {
+# input:
+#   psi : matrix of parameters (3 columns, ymax, xmax, slope)
+#   id : vector of indices 
+#   xidep : dependent variables (same nb of rows as length of id)
+# returns:
+#   a vector of predictions of length equal to length of id
+  x<-xidep[,1]
+  ymax<-psi[id,1]
+  xmax<-psi[id,2]
+  slope<-psi[id,3]
+  f<-ymax+slope*(x-xmax)
+#  cat(length(f),"  ",length(ymax),"\n")
+  f[x>xmax]<-ymax[x>xmax]
+  return(f)
+}
+saemix.model<-saemixModel(model=yield.LP,description="Linear plus plateau model",   
+  psi0=matrix(c(8,100,0.2,0,0,0),ncol=3,byrow=TRUE,dimnames=list(NULL,   
+  c("Ymax","Xmax","slope"))),covariate.model=matrix(c(0,0,0),ncol=3,byrow=TRUE), 
+  transform.par=c(0,0,0),covariance.model=matrix(c(1,0,0,0,1,0,0,0,1),ncol=3, 
+  byrow=TRUE),error.model="constant")
+
+saemix.options_rwm<-list(seed=39546,map=F,fim=F,ll.is=F, nb.chains = 1, nbiter.mcmc = c(iter_mcmc,0,0,0))
+saemix.options_linear<-list(seed=39546,map=F,fim=F,ll.is=F, nb.chains = 1, nbiter.mcmc = c(1,0,0,iter_mcmc))
+
+
+post_rwm<-saemix_newkernel(saemix.model,saemix.data,saemix.options_rwm)$post_rwm
+post_newkernel<-saemix_newkernel(saemix.model,saemix.data,saemix.options_linear)$post_newkernel
+
+
+
+final_rwm <- post_rwm[[1]]
+for (i in 2:length(post_rwm)) {
+  final_rwm <- rbind(final_rwm, post_rwm[[i]])
+}
+
+
+final_newkernel <- post_newkernel[[1]]
+for (i in 2:length(post_newkernel)) {
+  final_newkernel <- rbind(final_newkernel, post_newkernel[[i]])
+}
+
+
+#ALl individual posteriors
+graphConvMC_new(final_rwm, title="RWM")
+graphConvMC_new(final_newkernel, title="VB Linear case")
+#first individual posteriors
+graphConvMC_new(post_rwm[[1]], title="EM")
+
+graphConvMC_twokernels(final_rwm,final_newkernel, title="EM")
+graphConvMC_twokernels(post_rwm[[1]],post_newkernel[[1]], title="EM")
+
+
+
+
+# theo.onlypop<-saemix(saemix.model,saemix.data,saemix.options)
+
+# saemix.fit<-saemix(saemix.model,saemix.data,saemix.options)
+# plot(saemix.fit,plot.type="individual")
+
