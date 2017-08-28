ee237d255ca14c18fbebe63db648849f68dd28ef
Belhal KARIMI <Belhal.karimi@gmail.com>
2017-08-28 15:28:09 +0200

    cat

--- warfarin_cat/new_cat_data.R
+++ warfarin_cat/new_cat_data.R
@@ -118,3 +118,41 @@ graphConvMC_saem(cat_saem, title="new kernel")
 graphConvMC2_saem(theo_ref,cat_saem, title="new kernel")
 
 
+index = 1
+graphConvMC_twokernels(post_rwm[[index]],post_rwm[[index]], title="rwm vs foce")
+graphConvMC_twokernels(post_rwm[[index]],post_foce[[index]], title="rwm vs foce")
+
+
+final_rwm <- post_rwm[[1]]
+for (i in 2:length(post_rwm)) {
+  final_rwm <- rbind(final_rwm, post_rwm[[i]])
+}
+
+
+final_foce <- post_foce[[1]]
+for (i in 2:length(post_foce)) {
+  final_foce <- rbind(final_foce, post_foce[[i]])
+}
+
+
+
+graphConvMC_twokernels(final_rwm,final_rwm, title="EM")
+graphConvMC_twokernels(final_rwm,final_foce, title="EM")
+
+
+#Autocorrelation
+rwm.obj <- as.mcmc(post_rwm[[1]])
+corr_rwm <- autocorr(rwm.obj[,2])
+autocorr.plot(rwm.obj[,2])
+
+foce.obj <- as.mcmc(post_foce[[1]])
+corr_foce <- autocorr(foce.obj[,2])
+autocorr.plot(foce.obj[,2])
+
+
+#MSJD
+mssd(post_rwm[[index]][,2])
+mssd(post_foce[[index]][,2])
+
+
+
