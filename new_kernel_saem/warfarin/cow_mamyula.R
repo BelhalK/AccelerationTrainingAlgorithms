701064a50731593875844b64553c999ca59fe1c0
Belhal KARIMI <Belhal.karimi@gmail.com>
2017-11-14 23:17:15 +0100

    cat

--- new_kernel_saem/warfarin/cow_mamyula.R
+++ new_kernel_saem/warfarin/cow_mamyula.R
@@ -83,27 +83,27 @@ saemix.data<-saemixData(name.data=cow.saemix,header=TRUE,name.group=c("cow"),
 #   units=list(x="yr",y="cm"))
 
 
-growthcow<-function(psi,id,xidep) {
-# input:
-#   psi : matrix of parameters (3 columns, a, b, k)
-#   id : vector of indices 
-#   xidep : dependent variables (same nb of rows as length of id)
-# returns:
-#   a vector of predictions of length equal to length of id
-  x<-xidep[,1]
-  a<-psi[id,1]
-  b<-psi[id,2]
-  k<-psi[id,3]
-  f<-a*(1-b*exp(-k*x))
-  return(f)
-}
-saemix.model<-saemixModel(model=growthcow,
-  description="Exponential growth model", 
-  psi0=matrix(c(700,0.9,0.02,0,0,0),ncol=3,byrow=TRUE, 
-  dimnames=list(NULL,c("A","B","k"))),transform.par=c(1,1,1),fixed.estim=c(1,1,1), 
-  covariate.model=matrix(c(0,0,0),ncol=3,byrow=TRUE), 
-  covariance.model=matrix(c(1,0,0,0,1,0,0,0,1),ncol=3,byrow=TRUE), 
-  omega.init=matrix(c(1,0,0,0,1,0,0,0,1),ncol=3,byrow=TRUE),error.model="constant")
+# growthcow<-function(psi,id,xidep) {
+# # input:
+# #   psi : matrix of parameters (3 columns, a, b, k)
+# #   id : vector of indices 
+# #   xidep : dependent variables (same nb of rows as length of id)
+# # returns:
+# #   a vector of predictions of length equal to length of id
+#   x<-xidep[,1]
+#   a<-psi[id,1]
+#   b<-psi[id,2]
+#   k<-psi[id,3]
+#   f<-a*(1-b*exp(-k*x))
+#   return(f)
+# }
+# saemix.model<-saemixModel(model=growthcow,
+#   description="Exponential growth model", 
+#   psi0=matrix(c(700,0.9,0.02,0,0,0),ncol=3,byrow=TRUE, 
+#   dimnames=list(NULL,c("A","B","k"))),transform.par=c(1,1,1),fixed.estim=c(1,1,1), 
+#   covariate.model=matrix(c(0,0,0),ncol=3,byrow=TRUE), 
+#   covariance.model=matrix(c(1,0,0,0,1,0,0,0,1),ncol=3,byrow=TRUE), 
+#   omega.init=matrix(c(1,0,0,0,1,0,0,0,1),ncol=3,byrow=TRUE),error.model="constant")
 
 
 
