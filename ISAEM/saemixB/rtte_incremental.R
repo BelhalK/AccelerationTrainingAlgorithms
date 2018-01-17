100939e40960ad7ca454dd8e74c5e8d9146e33ab
Belhal KARIMI <Belhal.karimi@gmail.com>
2018-01-17 15:10:05 +0100

    incr

--- ISAEM/saemixB/rtte_incremental.R
+++ ISAEM/saemixB/rtte_incremental.R
@@ -25,7 +25,15 @@ setwd("/Users/karimimohammedbelhal/Documents/GitHub/saem/ISAEM/saemixB/R")
   source('mixtureFunctions.R')
 
 setwd("/Users/karimimohammedbelhal/Documents/GitHub/saem/ISAEM/saemixB/")
-
+source("/Users/karimimohammedbelhal/Desktop/papers/iem_code/imcem_saemix/plots_se.R")
+library("mlxR")
+library("psych")
+library("coda")
+library("Matrix")
+library(abind)
+require(ggplot2)
+require(gridExtra)
+require(reshape2)
 ###RTTE
 timetoevent.saemix <- read.table("/Users/karimimohammedbelhal/Documents/GitHub/saem/ISAEM/saemixB/data/rtte_data.csv", header=T, sep=",")
 # timetoevent.saemix <- read.table("/Users/karimimohammedbelhal/Desktop/package_contrib/saemixB/data/rttellis.csv", header=T, sep=",")
@@ -59,39 +67,195 @@ saemix.model_rtte<-saemixModel(model=timetoevent.model,description="time model",
 
 ##RUNS
 
-K1 = 500
-K2 = 200
+K1 = 300
+K2 = 100
 iterations = 1:(K1+K2+1)
 end = K1+K2
-batchsize<-50
+batchsize50<-50
+batchsize25<-25
 
 #Weibull
 options_rtte<-list(seed=39546,map=F,fim=F,ll.is=F,nbiter.mcmc = c(2,2,2,0), nbiter.saemix = c(K1,K2),nbiter.sa=0,displayProgress=TRUE,nbiter.burn =0, map.range=c(0))
 rtte<-data.frame(saemix(saemix.model_rtte,saemix.data_rtte,options_rtte))
 rtte<-cbind(iterations,rtte)
 
-options_rtteincr<-list(seed=39546,map=F,fim=F,ll.is=F,nbiter.mcmc = c(2,2,2,0), nbiter.saemix = c(K1,K2),nbiter.sa=0,displayProgress=TRUE,nbiter.burn =0, map.range=c(0), nb.replacement=batchsize)
+options_rtteincr<-list(seed=39546,map=F,fim=F,ll.is=F,nbiter.mcmc = c(2,2,2,0), nbiter.saemix = c(K1,K2),nbiter.sa=0,displayProgress=TRUE,nbiter.burn =0, map.range=c(0), nb.replacement=batchsize50)
 rtteincr<-data.frame(saemix_incremental(saemix.model_rtte,saemix.data_rtte,options_rtteincr))
 rtteincr<-cbind(iterations,rtteincr)
 
-graphConvMC2_saem(rtte,rtteincr, title="new kernel")
+options_rtteincr25<-list(seed=39546,map=F,fim=F,ll.is=F,nbiter.mcmc = c(2,2,2,0), nbiter.saemix = c(K1,K2),nbiter.sa=0,displayProgress=TRUE,nbiter.burn =0, map.range=c(0), nb.replacement=batchsize25)
+rtteincr25<-data.frame(saemix_incremental(saemix.model_rtte,saemix.data_rtte,options_rtteincr25))
+rtteincr25<-cbind(iterations,rtteincr25)
+
+# graphConvMC2_saem(rtte,rtteincr, title="new kernel")
 
 
 rtte$algo <- 'rwm'
-rtteincr$algo <- 'ISAEM'
+rtteincr$algo <- 'ISAEM50'
+rtteincr25$algo <- 'ISAEM25'
 
-rtte_scaled <- rtte[rep(seq_len(nrow(rtte)), each=100/batchsize),]
+rtte_scaled <- rtte[rep(seq_len(nrow(rtte)), each=100/batchsize25),]
 rtte_scaled$iterations = 1:(2*(K1+K2+1))
 
+rtte_scaled50 <- rtteincr[rep(seq_len(nrow(rtte)), each=100/batchsize50),]
+rtte_scaled50$iterations = 1:(2*(K1+K2+1))
 
 comparison <- 0
 # comparison <- rbind(theo_ref,theo_incremental)
+comparison <- rbind(rtte_scaled[iterations,],rtte_scaled50[iterations,],rtteincr25)
 comparison <- rbind(rtte_scaled[iterations,],rtteincr)
 
 var <- melt(comparison, id.var = c('iterations','algo'), na.rm = TRUE)
 graphConvMC3_new(var, title="ALGO - EM (same complexity)",legend=TRUE)
 
-options_rttenew<-list(seed=39546,map=F,fim=F,ll.is=F,nbiter.mcmc = c(2,2,2,6), nb.chains=1, nbiter.saemix = c(K1,K2),nbiter.sa=0,displayProgress=TRUE,nbiter.burn =0,map.range=c(1:5))
-rttenew<-data.frame(saemix(saemix.model_rtte,saemix.data_rtte,options_rttenew))
+# options_rttenew<-list(seed=39546,map=F,fim=F,ll.is=F,nbiter.mcmc = c(2,2,2,6), nb.chains=1, nbiter.saemix = c(K1,K2),nbiter.sa=0,displayProgress=TRUE,nbiter.burn =0,map.range=c(1:5))
+# rttenew<-data.frame(saemix(saemix.model_rtte,saemix.data_rtte,options_rttenew))
+
+
+#MC STUDY
+
+
+final_rwm <- 0
+final_ref <- 0
+var_rwm <- 0
+error_rwm <- 0
+final_mix <- 0
+final_mix25 <- 0
+var_mix <- 0
+var_mix25 <- 0
+error_mix <- 0
+error_mix25 <- 0
+
+
+lambda_true <- 2
+o_lambda_true <- 0.3
+beta_true <- 2
+o_beta_true <- 0.3
+final_mix <- 0
+true_param <- c(lambda_true,beta_true,o_lambda_true,o_beta_true)
+
+
+seed0 = 39546
+replicate = 20
+
+for (j in 1:replicate){
+
+     model2 <- inlineModel("
+
+  [LONGITUDINAL]
+  input = {beta,lambda}  
+
+  EQUATION:
+  h=(beta/lambda)*(t/lambda)^(beta-1)
+
+  DEFINITION:
+  e = {type               = event, 
+       rightCensoringTime = 6,  
+       hazard             = h}
+  [INDIVIDUAL]
+  input={lambda_pop, o_lambda,beta_pop, o_beta}
+                        
+  DEFINITION:
+  lambda  ={distribution=lognormal, prediction=lambda_pop,  sd=o_lambda}
+  beta  ={distribution=lognormal, prediction=beta_pop,  sd=o_beta}
+       ")
+
+
+  p <- c(lambda_pop=lambda_true, o_lambda= o_lambda_true,
+         beta_pop = beta_true,o_beta = o_beta_true)
+  h <- list(name='h', time=seq(0, 6, by=1))
+  e <- list(name='e', time=0)
+
+  N <- 50
+  res <- simulx(model     = model2, 
+                settings  = list(seed=j*123),
+                parameter = p, 
+                output    = list(h,e), 
+                 group     = list(size = N))
+  
+
+writeDatamlx(res, result.file = "/Users/karimimohammedbelhal/Documents/GitHub/saem/ISAEM/saemixB/data/incr_rtte.csv")
+head(read.table("/Users/karimimohammedbelhal/Documents/GitHub/saem/ISAEM/saemixB/data/incr_rtte.csv", header=T, sep=","))
+  timetoevent.saemix <- read.table("/Users/karimimohammedbelhal/Documents/GitHub/saem/ISAEM/saemixB/data/incr_rtte.csv", header=T, sep=",")
+  timetoevent.saemix <- timetoevent.saemix[timetoevent.saemix$ytype==2,]
+
+  saemix.data<-saemixData(name.data=timetoevent.saemix,header=TRUE,sep=" ",na=NA, name.group=c("id"),name.response=c("y"),name.predictors=c("time","y"), name.X=c("time"))
+
+
+saemix.model<-saemixModel(model=timetoevent.model,description="time model",type="likelihood",   
+  psi0=matrix(c(2,1),ncol=2,byrow=TRUE,dimnames=list(NULL,   
+  c("lambda","beta"))), 
+  transform.par=c(1,1),covariance.model=matrix(c(1,0,0,1),ncol=2, 
+  byrow=TRUE))
+  print(j)
+
+  options<-list(seed=39546,map=F,fim=F,ll.is=F,nbiter.mcmc = c(2,2,2,0), nbiter.saemix = c(K1,K2),nbiter.sa=0,displayProgress=TRUE,nbiter.burn =0, map.range=c(0), nb.replacement=100)
+  theo_ref<-data.frame(saemix_incremental(saemix.model,saemix.data,options))
+  theo_ref <- cbind(iterations, theo_ref)
+  var_rwm <- var_rwm + (theo_ref[,2:5]-true_param)^2
+  ML <- theo_ref[,2:5]
+  ML[1:(end+1),]<- theo_ref[end+1,2:5]
+  error_rwm <- error_rwm + (theo_ref[,2:5]-ML)^2
+  theo_ref['individual'] <- j
+  final_ref <- rbind(final_ref,theo_ref)
+
+
+  options.incremental<-list(seed=39546,map=F,fim=F,ll.is=F,nbiter.mcmc = c(2,2,2,0), nbiter.saemix = c(K1,K2),nbiter.sa=0,displayProgress=TRUE,nbiter.burn =0, map.range=c(0), nb.replacement=batchsize50)
+  theo_mix<-data.frame(saemix_incremental(saemix.model,saemix.data,options.incremental))
+  theo_mix <- cbind(iterations, theo_mix)
+
+  var_mix <- var_mix + (theo_mix[,2:5]-true_param)^2
+  ML <- theo_mix[,2:5]
+  ML[1:(end+1),]<- theo_mix[end+1,2:5]
+  error_mix <- error_mix + (theo_mix[,2:5]-ML)^2
+  theo_mix['individual'] <- j
+  final_mix <- rbind(final_mix,theo_mix)
+  
+  options.incremental25<-list(seed=39546,map=F,fim=F,ll.is=F,nbiter.mcmc = c(2,2,2,0), nbiter.saemix = c(K1,K2),nbiter.sa=0,displayProgress=TRUE,nbiter.burn =0, map.range=c(0), nb.replacement=batchsize25)
+  theo_mix25<-data.frame(saemix_incremental(saemix.model,saemix.data,options.incremental25))
+  theo_mix25 <- cbind(iterations, theo_mix25)
+  
+  var_mix25 <- var_mix25 + (theo_mix25[,2:5]-true_param)^2
+  ML <- theo_mix25[,2:5]
+  ML[1:(end+1),]<- theo_mix25[end+1,2:5]
+  error_mix25 <- error_mix25 + (theo_mix25[,2:5]-ML)^2
+  theo_mix25['individual'] <- j
+  final_mix25 <- rbind(final_mix25,theo_mix25)
+}
+
+
+graphConvMC_diff(final_ref,final_ref,final_ref)
+graphConvMC_diff(final_ref,final_mix,final_mix25)
+
+error_rwm <- 1/replicate*error_rwm
+error_mix <- 1/replicate*error_mix
+error_mix25 <- 1/replicate*error_mix25
+
+error_rwm <- cbind(iterations, error_rwm)
+error_mix <- cbind(iterations, error_mix)
+error_mix25 <- cbind(iterations, error_mix25)
+
+err_mix<- theo_ref
+err_rwm<- theo_ref
+err_mix25<- theo_ref
+
+
+err_rwm[,2:5] <- error_rwm[,2:5]
+err_mix[,2:5] <- error_mix[,2:5]
+err_mix25[,2:5] <- error_mix25[,2:5]
+
+err_mix[2,] = err_rwm[2,]=err_mix25[2,]
+
+err_rwm_scaled <- err_rwm[rep(seq_len(nrow(err_rwm)), each=4),]
+err_mix_scaled <- err_mix[rep(seq_len(nrow(err_mix)), each=2),]
+err_rwm_scaled$iterations = 1:(4*(K1+K2+1))
+err_mix_scaled$iterations = 1:(2*(K1+K2+1))
+
+
+# c <- graphConvMC_se2(err_rwm_scaled[,c(1,2,8)],err_rwm_scaled[,c(1,2,8)],err_rwm_scaled[,c(1,2,8)])
+c <- graphConvMC_sec(err_rwm_scaled[2:end,c(1,2,6)],err_mix_scaled[2:end,c(1,2,6)],err_mix25[2:end,c(1,2,6)])
+d <- graphConvMC_sed(err_rwm_scaled[2:end,c(1,4,6)],err_mix_scaled[2:end,c(1,4,6)],err_mix25[2:end,c(1,4,6)])
+
+grid.arrange(c,d, ncol=2)
 
 
--- ISAEM/saemixB/warfa_incremental.R
+++ ISAEM/saemixB/warfa_incremental.R
@@ -23,7 +23,20 @@ setwd("/Users/karimimohammedbelhal/Documents/GitHub/saem/ISAEM/saemixB/R")
   source('main_incremental.R')
   source('main_estep_incremental.R')
 
+  source('/Users/karimimohammedbelhal/Documents/GitHub/saem/ISAEM/saemixB/R/mixtureFunctions.R')
+  source("/Users/karimimohammedbelhal/Desktop/papers/iem_code/imcem_saemix/plots_se.R")
 setwd("/Users/karimimohammedbelhal/Documents/GitHub/saem/ISAEM/saemixB")
+
+
+library("mlxR")
+library("psych")
+library("coda")
+library("Matrix")
+library(abind)
+require(ggplot2)
+require(gridExtra)
+require(reshape2)
+
 warfa_data <- read.table("/Users/karimimohammedbelhal/Documents/GitHub/saem/ISAEM/saemixB/data/warfarin_data.txt", header=T)
 saemix.data_warfa<-saemixData(name.data=warfa_data,header=TRUE,sep=" ",na=NA, name.group=c("id"),
   name.predictors=c("amount","time"),name.response=c("y1"), name.X="time")
@@ -51,28 +64,37 @@ K1 = 300
 K2 = 200
 iterations = 1:(K1+K2+1)
 end = K1+K2
-batchsize = 25
+batchsize25 = 25
+batchsize50 = 50
 #Warfarin
 options_warfa<-list(seed=39546,map=F,fim=F,ll.is=F,nbiter.mcmc = c(2,2,2,0),nb.chains=1, nbiter.saemix = c(K1,K2),nbiter.sa=0,displayProgress=TRUE,nbiter.burn =0, map.range=c(0))
 warfa<-data.frame(saemix(saemix.model_warfa,saemix.data_warfa,options_warfa))
 warfa<-cbind(iterations,warfa)
 
-options_warfaincr<-list(seed=39546,map=F,fim=F,ll.is=F,nbiter.mcmc = c(2,2,2,0),nb.chains=1, nbiter.saemix = c(K1,K2),nbiter.sa=0,displayProgress=TRUE,nbiter.burn =0, map.range=c(0), nb.replacement=batchsize)
-warfaincr<-data.frame(saemix_incremental(saemix.model_warfa,saemix.data_warfa,options_warfaincr))
-warfaincr<-cbind(iterations,warfaincr)
+options_warfaincr25<-list(seed=39546,map=F,fim=F,ll.is=F,nbiter.mcmc = c(2,2,2,0),nb.chains=1, nbiter.saemix = c(K1,K2),nbiter.sa=0,displayProgress=TRUE,nbiter.burn =0, map.range=c(0), nb.replacement=batchsize25)
+warfaincr25<-data.frame(saemix_incremental(saemix.model_warfa,saemix.data_warfa,options_warfaincr25))
+warfaincr25<-cbind(iterations,warfaincr25)
+
+options_warfaincr50<-list(seed=39546,map=F,fim=F,ll.is=F,nbiter.mcmc = c(2,2,2,0),nb.chains=1, nbiter.saemix = c(K1,K2),nbiter.sa=0,displayProgress=TRUE,nbiter.burn =0, map.range=c(0), nb.replacement=batchsize50)
+warfaincr50<-data.frame(saemix_incremental(saemix.model_warfa,saemix.data_warfa,options_warfaincr50))
+warfaincr50<-cbind(iterations,warfaincr50)
 
 graphConvMC2_saem(warfa,warfaincr, title="new kernel")
 
 warfa$algo <- 'rwm'
-warfaincr$algo <- 'ISAEM'
+warfaincr25$algo <- 'ISAEM25'
+warfaincr50$algo <- 'ISAEM50'
 
-warfa_scaled <- warfa[rep(seq_len(nrow(warfa)), each=100/batchsize),]
-warfa_scaled$iterations = 1:(2*(K1+K2+1))
+warfa_scaled <- warfa[rep(seq_len(nrow(warfa)), each=100/batchsize25),]
+warfa_scaled$iterations = 1:(4*(K1+K2+1))
 
+warfa_scaled50 <- warfaincr50[rep(seq_len(nrow(warfaincr50)), each=100/batchsize50),]
+warfa_scaled50$iterations = 1:(2*(K1+K2+1))
 
 comparison <- 0
 # comparison <- rbind(theo_ref,theo_incremental)
-comparison <- rbind(warfa_scaled[iterations,],warfaincr)
+# comparison <- rbind(warfa_scaled[iterations,],warfaincr)
+comparison <- rbind(warfa_scaled[iterations,],warfa_scaled50[iterations,],warfaincr25)
 
 var <- melt(comparison, id.var = c('iterations','algo'), na.rm = TRUE)
 graphConvMC3_new(var, title="ALGO - EM (same complexity)",legend=TRUE)
@@ -80,3 +102,180 @@ graphConvMC3_new(var, title="ALGO - EM (same complexity)",legend=TRUE)
 
 options_warfanew<-list(seed=39546,map=F,fim=F,ll.is=F,nbiter.mcmc = c(2,2,2,6), nb.chains=1, nbiter.saemix = c(K1,K2),nbiter.sa=0,displayProgress=TRUE,nbiter.burn =0, map.range=c(1:3))
 warfanew<-data.frame(saemix(saemix.model_warfa,saemix.data_warfa,options_warfanew))
+
+
+final_rwm <- 0
+final_ref <- 0
+var_rwm <- 0
+error_rwm <- 0
+final_mix <- 0
+final_mix25 <- 0
+var_mix <- 0
+var_mix25 <- 0
+error_mix <- 0
+error_mix25 <- 0
+
+
+ka_true <- 1
+V_true <- 8
+k_true <- 0.1/8
+o_ka <- 0.5
+o_V <- 0.2
+o_k <- 0.3
+
+true_param <- c(ka_true,V_true,k_true,o_ka,o_V,o_k)
+
+seed0 = 39546
+replicate = 20
+replicate <- 4
+for (m in 1:replicate){
+  
+    model<-"/Users/karimimohammedbelhal/Desktop/CSDA_code_ref/warfarin/warfarin_project_model.txt"
+# treatment
+trt <- read.table("/Users/karimimohammedbelhal/Desktop/CSDA_code_ref/warfarin/treatment.txt", header = TRUE) 
+
+# parameters 
+originalId<- read.table('/Users/karimimohammedbelhal/Desktop/CSDA_code_ref/warfarin/originalId.txt', header=TRUE) 
+populationParameter<- read.vector('/Users/karimimohammedbelhal/Desktop/CSDA_code_ref/warfarin/populationParameter2.txt') 
+individualCovariate<- read.table('/Users/karimimohammedbelhal/Desktop/CSDA_code_ref/warfarin/individualCovariate.txt', header = TRUE) 
+list.param <- list(populationParameter,individualCovariate)
+# output 
+name<-"y1"
+time<-read.table("/Users/karimimohammedbelhal/Desktop/CSDA_code_ref/warfarin/output1.txt",header=TRUE)
+out1<-list(name=name,time=time) 
+name<-"y2"
+time<-read.table("/Users/karimimohammedbelhal/Desktop/CSDA_code_ref/warfarin/output2.txt",header=TRUE)
+out2<-list(name=name,time=time) 
+out<-list(out1,out2)
+
+# call the simulator 
+res <- simulx(model=model,treatment=trt,parameter=list.param,output=out)
+# warfarin.saemix <- data(res)
+
+# writeDatamlx(res, result.file = "/Users/karimimohammedbelhal/Documents/GitHub/saem/new_kernel_saem/warfarin/war_synth.csv")
+# table <- read.table("/Users/karimimohammedbelhal/Documents/GitHub/saem/new_kernel_saem/warfarin/war_synth.csv", header=T, sep=",")
+# table <- table[table$ytype==1,]
+
+# table[,5] <- 0
+
+warfarin.saemix <- res$y1
+warfarin.saemix["amount"] <- 0
+treat <- res$treatment
+treat["y1"] <- 0
+treat <- treat[c(1,2,4,3)]
+
+j <- 1
+l<-c()
+
+
+for (i in 1:241) {
+    
+    if(t(warfarin.saemix["id"])[i]==t(treat["id"])[j]){
+        print(rownames(warfarin.saemix[i,]))
+        l <- rbind(l,rownames(warfarin.saemix[i,]))
+        j<-j+1
+      }
+}
+
+warfarin.saemix <- rbind(treat[1,], warfarin.saemix)
+warfarin.saemix[1:7,4] <- treat[1,4]
+j <- 2
+for (i in l[-1]){
+  warfarin.saemix[(as.numeric(i)+1):(as.numeric(i)+length(which(t(warfarin.saemix["id"]==j)))),4] <- treat[j,4]
+  warfarin.saemix <- rbind(warfarin.saemix[1:(as.numeric(i)-1),], treat[j,], warfarin.saemix[(as.numeric(i)+1):nrow(warfarin.saemix),])
+  j <- j +1
+}
+
+rownames(warfarin.saemix) <- 1:nrow(warfarin.saemix)
+# warfarin.saemix <- table[c(1,2,3,5)]
+warfarin.saemix_less <- warfarin.saemix[,]
+  saemix.model<-saemixModel(model=model1cpt,description="warfarin",type="structural"
+  ,psi0=matrix(c(1,7,1,0,0,0),ncol=3,byrow=TRUE, dimnames=list(NULL, c("ka","V","k"))),
+  transform.par=c(1,1,1),omega.init=matrix(c(1,0,0,0,1,0,0,0,1),ncol=3,byrow=TRUE),
+  covariance.model=matrix(c(1,0,0,0,1,0,0,0,1),ncol=3, 
+  byrow=TRUE))
+
+
+saemix.data<-saemixData(name.data=warfarin.saemix_less,header=TRUE,sep=" ",na=NA, name.group=c("id"),
+  name.predictors=c("amount","time"),name.response=c("y1"), name.X="time")
+
+
+
+options<-list(seed=39546,map=F,fim=F,ll.is=F,nbiter.mcmc = c(2,2,2,0), nbiter.saemix = c(K1,K2),nbiter.sa=0,displayProgress=TRUE,nbiter.burn =0, map.range=c(0), nb.replacement=100)
+theo_ref<-data.frame(saemix_incremental(saemix.model,saemix.data,options))
+theo_ref <- cbind(iterations, theo_ref)
+var_rwm <- var_rwm + (theo_ref[,2:8]-true_param)^2
+ML <- theo_ref[,2:8]
+ML[1:(end+1),]<- theo_ref[end+1,2:8]
+error_rwm <- error_rwm + (theo_ref[,2:8]-ML)^2
+theo_ref['individual'] <- j
+final_ref <- rbind(final_ref,theo_ref)
+
+
+options.incremental<-list(seed=39546,map=F,fim=F,ll.is=F,nbiter.mcmc = c(2,2,2,0), nbiter.saemix = c(K1,K2),nbiter.sa=0,displayProgress=TRUE,nbiter.burn =0, map.range=c(0), nb.replacement=batchsize50)
+theo_mix<-data.frame(saemix_incremental(saemix.model,saemix.data,options.incremental))
+theo_mix <- cbind(iterations, theo_mix)
+
+var_mix <- var_mix + (theo_mix[,2:8]-true_param)^2
+ML <- theo_mix[,2:8]
+ML[1:(end+1),]<- theo_mix[end+1,2:8]
+error_mix <- error_mix + (theo_mix[,2:8]-ML)^2
+theo_mix['individual'] <- j
+final_mix <- rbind(final_mix,theo_mix)
+
+options.incremental25<-list(seed=39546,map=F,fim=F,ll.is=F,nbiter.mcmc = c(2,2,2,0), nbiter.saemix = c(K1,K2),nbiter.sa=0,displayProgress=TRUE,nbiter.burn =0, map.range=c(0), nb.replacement=batchsize25)
+theo_mix25<-data.frame(saemix_incremental(saemix.model,saemix.data,options.incremental25))
+theo_mix25 <- cbind(iterations, theo_mix25)
+
+var_mix25 <- var_mix25 + (theo_mix25[,2:8]-true_param)^2
+ML <- theo_mix25[,2:8]
+ML[1:(end+1),]<- theo_mix25[end+1,2:8]
+error_mix25 <- error_mix25 + (theo_mix25[,2:8]-ML)^2
+theo_mix25['individual'] <- j
+final_mix25 <- rbind(final_mix25,theo_mix25)
+
+
+ 
+}
+
+graphConvMC_diff(final_ref,final_ref,final_ref)
+graphConvMC_diff(final_ref,final_mix,final_mix25)
+
+error_rwm <- 1/replicate*error_rwm
+error_mix <- 1/replicate*error_mix
+
+error_rwm <- cbind(iterations, error_rwm)
+error_mix <- cbind(iterations, error_mix)
+
+err_mix<- theo_ref
+err_rwm<- theo_ref
+err_rwm[,2:8] <- error_rwm[,2:8]
+err_mix[,2:8] <- error_mix[,2:8]
+
+err_mix[2,] = err_rwm[2,]
+
+
+# graphConvMC_diff(err_rwm[-1,],err_mix[-1,], title="Quadratic errors Warfa")
+# graphConvMC_diff2(err_rwm[-1,],err_mix[-1,], title="Quadratic errors Warfa")
+
+# graphConvMC_diff2(err_rwm[-1,c(1,3,6,9)],err_mix[-1,c(1,3,6,9)])
+
+a <- err_rwm[-1,]
+b <- err_mix[-1,]
+# graphConvMC_diff2(err_rwm[-1,c(1,3,6,9)],err_mix[-1,c(1,3,6,9)], title="Quadratic errors Warfa")
+
+# err_rwm[,2:8] <- sqrt(err_rwm[,2:8])
+# err_mix[,2:8] <- sqrt(err_mix[,2:8])
+
+# graphConvMC_diff2(a[,c(1,3,6,9)],b[,c(1,3,6,9)], title="Quadratic errors Warfa")
+# graphConvMC_diff2(err_rwm[-1,c(1,3,6,9)],err_mix[-1,c(1,3,6,9)], title="Quadratic errors Warfa")
+
+# graphConvMC_diff2(a[,c(1,3,6,9)],b[,c(1,3,6,9)])
+
+c <- graphConvMC_diff3(a[,c(1,3,9)],b[,c(1,3,9)])
+d <- graphConvMC_diff3(a[,c(1,6,9)],b[,c(1,6,9)])
+
+grid.arrange(c,d, ncol=2)
+
+
+
