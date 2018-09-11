7ca09a4b2e585c0f1010858dbb5b3d5788eddc50
BelhalK <belhal.karimi@gmail.com>
2018-09-11 16:22:19 +0200

    isaem"

--- ISAEM/incremental/isaem_pk_simu_newkernel.R
+++ ISAEM/incremental/isaem_pk_simu_newkernel.R
@@ -99,7 +99,7 @@ saemix.model<-saemixModel(model=model1cpt,description="warfarin",type="structura
 
 K1 = 1000
 K2 = 500
-iterations = 0:(K1+K2-1)
+iterations = 1:(K1+K2)
 end = K1+K2
 batchsize25 = 25
 batchsize50 = 50
@@ -108,24 +108,50 @@ seed0=3456
 
 
 
-# options<-list(seed=39546,map=F,fim=F,ll.is=F,save.graphs=FALSE,nbiter.mcmc = c(2,2,2,2),
-#  nbiter.saemix = c(K1,K2),nbiter.sa=0,displayProgress=FALSE,nbiter.burn =0, 
-#  map.range=c(1:4), nb.replacement=100,sampling='seq')
-# theo_ref<-saemix_incremental(saemix.model,saemix.data,options)
-# theo_ref <- data.frame(theo_ref$param)
-# theo_ref <- cbind(iterations, theo_ref[-1,])
-# # row_sub  = apply(theo_ref, 1, function(row) all(row !=0 ))
-# theo_ref <- theo_ref[row_sub,]
+options<-list(seed=39546,map=F,fim=F,ll.is=F,save.graphs=FALSE,nbiter.mcmc = c(2,2,2,2),
+ nbiter.saemix = c(K1,K2),nbiter.sa=0,displayProgress=FALSE,nbiter.burn =0, 
+ map.range=c(1:4), nb.replacement=100,sampling='seq')
+theo_ref<-saemix_incremental(saemix.model,saemix.data,options)
+theo_ref <- data.frame(theo_ref$param)
+theo_ref <- cbind(iterations, theo_ref[-1,])
+row_sub_ref  = apply(theo_ref, 1, function(row) all(row !=0 ))
+theo_ref <- theo_ref[row_sub_ref,]
+theo_ref$algo <- 'full'
+theo_ref$iterations <- seq(0,10, length.out=length(theo_ref$iterations))
 
 
-# options.incremental25<-list(seed=seed0,map=F,fim=F,ll.is=F,save.graphs=FALSE,nb.chains = 1, 
-#   nbiter.mcmc = c(2,2,2,2), nbiter.saemix = c(K1,K2),displayProgress=FALSE, map.range=c(1:4),
-#   nbiter.sa=0,nbiter.burn =0, nb.replacement=25,sampling='randompass')
-# theo_mix25<-saemix_incremental(saemix.model,saemix.data,options.incremental25)
-# theo_mix25 <- data.frame(theo_mix25$param)
-# theo_mix25 <- cbind(iterations, theo_mix25[-1,])
-# row_sub  = apply(theo_mix25, 1, function(row) all(row !=0 ))
-# theo_mix25 <- theo_mix25[row_sub,]
+options.incremental25<-list(seed=seed0,map=F,fim=F,ll.is=F,save.graphs=FALSE,nb.chains = 1, 
+  nbiter.mcmc = c(2,2,2,2), nbiter.saemix = c(K1,K2),displayProgress=FALSE, map.range=c(1:4),
+  nbiter.sa=0,nbiter.burn =0, nb.replacement=25,sampling='randompass')
+theo_mix25<-saemix_incremental(saemix.model,saemix.data,options.incremental25)
+theo_mix25 <- data.frame(theo_mix25$param)
+theo_mix25 <- cbind(iterations, theo_mix25[-1,])
+row_sub  = apply(theo_mix25, 1, function(row) all(row !=0 ))
+theo_mix25 <- theo_mix25[row_sub,]
+theo_mix25$algo <- 'quarter'
+theo_mix25$iterations <- seq(0,10, length.out=length(theo_mix25$iterations))
+
+
+comparison <- 0
+comparison <- rbind(theo_ref[,],theo_mix25[,])
+var <- melt(comparison, id.var = c('iterations','algo'), na.rm = TRUE)
+
+
+seplot <- function(df, title=NULL, ylim=NULL, legend=TRUE)
+{
+  G <- (ncol(df)-2)/3
+  df$algo <- as.factor(df$algo)
+  ylim <-rep(ylim,each=2)
+  graf <- vector("list", ncol(df)-2)
+  graf <- ggplot(df)+geom_line(aes(iterations,value,by=value,colour = df$algo),show.legend = legend) +
+  xlab("iterations") + ylab('value') + facet_wrap(~variable,scales = "free_y") + theme_bw() 
+  grid.arrange(graf)
+  # do.call("grid.arrange", c(graf, ncol=1, top=title))
+}
+
+
+prec <- seplot(var, title="comparison",legend=TRUE)
+assign(paste("prec", i, sep = ""), prec) 
 
 
 
