4ba6b5bfa621616a718d0834598dd6a2cde028f2
BelhalK <belhal.karimi@gmail.com>
2018-09-15 15:48:45 +0200

    isaem

--- ISAEM/incremental/isaem_pk_simu_newkernel.R
+++ ISAEM/incremental/isaem_pk_simu_newkernel.R
@@ -137,21 +137,21 @@ comparison <- rbind(theo_ref[,],theo_mix25[,])
 var <- melt(comparison, id.var = c('iterations','algo'), na.rm = TRUE)
 
 
-# seplot <- function(df, title=NULL, ylim=NULL, legend=TRUE)
-# {
-#   G <- (ncol(df)-2)/3
-#   df$algo <- as.factor(df$algo)
-#   ylim <-rep(ylim,each=2)
-#   graf <- vector("list", ncol(df)-2)
-#   graf <- ggplot(df)+geom_line(aes(iterations,value,by=value,colour = df$algo),show.legend = legend) +
-#   xlab("iterations") + ylab('value') + facet_wrap(~variable,scales = "free_y") + theme_bw() 
-#   grid.arrange(graf)
-#   # do.call("grid.arrange", c(graf, ncol=1, top=title))
-# }
+seplot <- function(df, title=NULL, ylim=NULL, legend=TRUE)
+{
+  G <- (ncol(df)-2)/3
+  df$algo <- as.factor(df$algo)
+  ylim <-rep(ylim,each=2)
+  graf <- vector("list", ncol(df)-2)
+  graf <- ggplot(df)+geom_line(aes(iterations,value,by=value,colour = df$algo),show.legend = legend) +
+  xlab("iterations") + ylab('value') + facet_wrap(~variable,scales = "free_y") + theme_bw() 
+  grid.arrange(graf)
+  # do.call("grid.arrange", c(graf, ncol=1, top=title))
+}
 
 
-# prec <- seplot(var, title="comparison",legend=TRUE)
-# assign(paste("prec", i, sep = ""), prec) 
+prec <- seplot(var, title="comparison",legend=TRUE)
+assign(paste("prec", i, sep = ""), prec) 
 
 
 
