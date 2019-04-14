93a4bb7bd808590d425c4473f9d55f928bba5a51
BelhalK <belhal.karimi@gmail.com>
2019-04-14 22:09:06 +0200

    maj

--- FastIEM/src/pLSA/plsa_all.py
+++ FastIEM/src/pLSA/plsa_all.py
@@ -211,13 +211,15 @@ def MStep_onlinevr(index):
             else:
                 lamda[i, k] /= denominator
 
-def MStepsaga(index_i, index_j):
-    # update theta
+
+def SAGAStep(index_i, index_j):
+    # compute new and old individual h_i
+    oldh = h
     for i in index_i:
         for j in range(0, M):
             denominator = 0;
             for k in range(0, K):
-                h[i, j, k] = listofthetas[i][k, j] * listoflamdas[i, k];
+                h[i, j, k] = theta[k, j] * lamda[i, k];
                 denominator += h[i, j, k];
             if denominator == 0:
                 for k in range(0, K):
@@ -225,16 +227,17 @@ def MStepsaga(index_i, index_j):
             else:
                 for k in range(0, K):
                     h[i, j, k] /= denominator;
+    
+    # update theta
     for k in range(0, K):
         denominator = 0
-        for j in range(0, M):
-            theta[k, j] = 0
+        for j in range(0, M):    
             tmp = 0
             for i in index_i:
-                tmp +=  X[i, j] * p[i, j, k] - X[i, j] * h[i, j, k]
-            Vsomme = Hsomme[k,j] + N*tmp
-            Ssomme[k,j] = (1- rho_saga)*Ssomme[k,j] + rho_saga*Vsomme
-            theta[k, j] = Ssomme[k,j]
+                tmp += X[i, j] *(h[i, j, k] - oldh[i, j, k])
+            Vsomme[k,j] = Hsomme[k,j] + N*tmp
+            Ssomme[k,j] = (1-rho_saga)*Ssomme[k,j] + rho_saga*Vsomme[k,j]
+            theta[k, j] = Ssomme[k, j]
             denominator += theta[k, j]
         if denominator == 0:
             for j in range(0, M): 
@@ -242,6 +245,7 @@ def MStepsaga(index_i, index_j):
         else:
             for j in range(0, M):
                 theta[k, j] /= denominator
+
     # update lamda
     for i in range(0, N):
         for k in range(0, K):
@@ -249,22 +253,77 @@ def MStepsaga(index_i, index_j):
             denominator = 0
             for j in range(0, M):
                 lamda[i, k] += X[i, j] * p[i, j, k]
-                # lamda[i, k] += 1
                 denominator += X[i, j];
             if denominator == 0:
                 lamda[i, k] = 1.0 / K
             else:
                 lamda[i, k] /= denominator
-    for j in range(0, M):
-        for k in range(0, K):
-            tmph = 0
+
+    for k in range(0, K):
+        for j in range(0, M):
             for i in index_j:
-                h[i, j, k] = listofthetas[i][k, j] * listoflamdas[i, k];
-                tmph +=  (X[i, j] * p[i, j, k] - X[i, j] * h[i, j, k])
-            Hsomme[k,j] += tmph
+                Hsomme[k,j] += X[i, j] *(theta[k, j] * lamda[i, k] - listofthetas[i][k, j] * listoflamdas[i, k]);
+    #save all parameters per individual
     for i in index_j:
         listofthetas[i] = theta
-        listoflamdas[i,] = lamda[i,]
+        for k in range(0, K):
+            listoflamdas[i,k] = lamda[i,k]
+
+
+# def MStepsaga(index_i, index_j):
+#     # update theta
+#     for i in index_i:
+#         for j in range(0, M):
+#             denominator = 0;
+#             for k in range(0, K):
+#                 h[i, j, k] = listofthetas[i][k, j] * listoflamdas[i, k];
+#                 denominator += h[i, j, k];
+#             if denominator == 0:
+#                 for k in range(0, K):
+#                     h[i, j, k] = 0;
+#             else:
+#                 for k in range(0, K):
+#                     h[i, j, k] /= denominator;
+#     for k in range(0, K):
+#         denominator = 0
+#         for j in range(0, M):
+#             theta[k, j] = 0
+#             tmp = 0
+#             for i in index_i:
+#                 tmp +=  X[i, j] * p[i, j, k] - X[i, j] * h[i, j, k]
+#             Vsomme = Hsomme[k,j] + N*tmp
+#             Ssomme[k,j] = (1- rho_saga)*Ssomme[k,j] + rho_saga*Vsomme
+#             theta[k, j] = Ssomme[k,j]
+#             denominator += theta[k, j]
+#         if denominator == 0:
+#             for j in range(0, M): 
+#                 theta[k, j] = 1.0 / M
+#         else:
+#             for j in range(0, M):
+#                 theta[k, j] /= denominator
+#     # update lamda
+#     for i in range(0, N):
+#         for k in range(0, K):
+#             lamda[i, k] = 0
+#             denominator = 0
+#             for j in range(0, M):
+#                 lamda[i, k] += X[i, j] * p[i, j, k]
+#                 # lamda[i, k] += 1
+#                 denominator += X[i, j];
+#             if denominator == 0:
+#                 lamda[i, k] = 1.0 / K
+#             else:
+#                 lamda[i, k] /= denominator
+#     for j in range(0, M):
+#         for k in range(0, K):
+#             tmph = 0
+#             for i in index_j:
+#                 h[i, j, k] = listofthetas[i][k, j] * listoflamdas[i, k];
+#                 tmph +=  (X[i, j] * p[i, j, k] - X[i, j] * h[i, j, k])
+#             Hsomme[k,j] += tmph
+#     for i in index_j:
+#         listofthetas[i] = theta
+#         listoflamdas[i,] = lamda[i,]
 
 
 # calculate the log likelihood
@@ -389,6 +448,7 @@ mini_batch_size = 4
 #REINITIALIZE
 lamda = np.random.sample([N, K])
 theta = np.random.sample([K, M])
+
 p = zeros([N, M, K])
 initializeParameters()
 oldLoglikelihood = 1
@@ -518,7 +578,7 @@ for epoch in range(0, nb_epochs):
             MStep_onlinevr(mini_batch)
         newLoglikelihood = LogLikelihood()
         print("[", time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())), "] ", epoch+1, " iteration  ", str(newLoglikelihood))
-        # if(oldLoglikelihood != 1 and newLoglikelihood - oldLoglikelihood < threshold):
+        # if(oldLoglikelihood != 1 and newLoglikelihood - old Loglikelihood < threshold):
         #     break
         objectiveoEM_vr.append(newLoglikelihood)
         oldLoglikelihood = newLoglikelihood
@@ -526,12 +586,11 @@ for epoch in range(0, nb_epochs):
 
 
 #### PLOTTING #######
-epochs = len(objectiveIEM)
-plt.plot(np.arange(epochs), objectiveIEM, label='IEM')
-plt.plot(np.arange(epochs), objectiveEM, label='oEM')
-plt.plot(np.arange(epochs), objectiveoEM, label='EM')
-plt.plot(np.arange(epochs), objectiveoEM_vr, label='oEMVR')
-plt.plot(np.arange(epochs), objectiveSAGA, label='FI-EM')
+plt.plot(np.arange(nb_epochs), objectiveIEM, label='IEM')
+plt.plot(np.arange(nb_epochs), objectiveEM, label='EM')
+plt.plot(np.arange(nb_epochs), objectiveoEM, label='oEM')
+plt.plot(np.arange(nb_epochs), objectiveoEM_vr, label='oEMVR')
+plt.plot(np.arange(nb_epochs), objectiveSAGA, label='FI-EM')
 leg = plt.legend(fontsize=20,fancybox=True, loc='right')
 leg.get_frame().set_alpha(0.5)
 plt.xlabel('Epoch', fontsize=15)
