f7f4c42dadc9c0fca616b3cb8f316f62283c4746
BelhalK <belhal.karimi@gmail.com>
2019-04-14 22:07:39 +0200

    maj

--- FastIEM/src/pLSA/debugsaga_3.py
+++ FastIEM/src/pLSA/debugsaga_3.py
@@ -14,7 +14,6 @@ def EStep():
                     p[i, j, k] /= denominator;
 
 
-
 def MStep():
     # update theta
     for k in range(0, K):
@@ -47,24 +46,22 @@ def MStep():
 
 
 def SAGAStep(index_i, index_j):
-    for i in range(0, N):
+    # compute new and old individual h_i
+    oldh = h
+    for i in index_i:
         for j in range(0, M):
             denominator = 0;
-            denominatoroldh = 0;
             for k in range(0, K):
-                if i in index_i:
-                    h[i, j, k] = X[i, j] *(theta[k, j] * lamda[i, k]);
-                    oldh[i, j, k] = X[i, j] * (listofthetas[i][k, j] * listoflamdas[i, k]);
+                h[i, j, k] = theta[k, j] * lamda[i, k];
                 denominator += h[i, j, k];
-                denominatoroldh += oldh[i, j, k];
             if denominator == 0:
                 for k in range(0, K):
                     h[i, j, k] = 0;
-                    oldh[i, j, k] = 0;
             else:
                 for k in range(0, K):
                     h[i, j, k] /= denominator;
-                    oldh[i, j, k] /= denominatoroldh;
+    
+    # update theta
     for k in range(0, K):
         denominator = 0
         for j in range(0, M):    
@@ -74,6 +71,7 @@ def SAGAStep(index_i, index_j):
             Vsomme[k,j] = Hsomme[k,j] + N*tmp
             Ssomme[k,j] = (1-rho_saga)*Ssomme[k,j] + rho_saga*Vsomme[k,j]
             theta[k, j] = Ssomme[k, j]
+            denominator += theta[k, j]
         if denominator == 0:
             for j in range(0, M): 
                 theta[k, j] = 1.0 / M
@@ -87,38 +85,32 @@ def SAGAStep(index_i, index_j):
             lamda[i, k] = 0
             denominator = 0
             for j in range(0, M):
-                lamda[i, k] += X[i, j] * h[i, j, k]
+                lamda[i, k] += X[i, j] * p[i, j, k]
                 denominator += X[i, j];
             if denominator == 0:
                 lamda[i, k] = 1.0 / K
             else:
                 lamda[i, k] /= denominator
+
     for k in range(0, K):
         for j in range(0, M):
             for i in index_j:
                 Hsomme[k,j] += X[i, j] *(theta[k, j] * lamda[i, k] - listofthetas[i][k, j] * listoflamdas[i, k]);
+    #save all parameters per individual
     for i in index_j:
         listofthetas[i] = theta
         for k in range(0, K):
             listoflamdas[i,k] = lamda[i,k]
 
 
-
-
-rho_saga = 0.001
+rho_saga = 0.01
 #REINITIALIZE
 lamda = np.random.sample([N, K])
 theta = np.random.sample([K, M])
 p = zeros([N, M, K])
 initializeParameters()
-# EM algorithm
 oldLoglikelihood = 1
 newLoglikelihood = 1
-# ## REINITIALIZE
-p = zeros([N, M, K])
-oldLoglikelihood = 1
-newLoglikelihood = 1
-
 
 ### SAGA EM
 objectiveSAGA = []
@@ -132,39 +124,39 @@ print("[", time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())), "] ",
 objectiveSAGA.append(newLoglikelihood)
 oldLoglikelihood = newLoglikelihood
 
+listofthetas = {}
+for i in range(N):
+    listofthetas[i] = theta
+listoflamdas = lamda
 
+h = p
+oldh = p
 for k in range(0, K):
     denominator = 0
     for j in range(0, M):
         Hsomme[k, j] = 0
         for i in range(0, N):
-            Hsomme[k, j] += X[i, j] * p[i, j, k]
-        denominator += Hsomme[k, j]
-    if denominator == 0:
-        for j in range(0, M): 
-            Hsomme[k, j] = 1.0 / M
-    else:
-        for j in range(0, M):
-            Hsomme[k, j] /= denominator
+            Hsomme[k, j] += X[i, j] * h[i, j, k]
 
 Ssomme = Hsomme
 Vsomme = Hsomme
-h = p 
-oldh = p
-listofthetas = {}
-for i in range(N):
-    listofthetas[i] = theta
-listoflamdas = lamda
+
 
 
 for epoch in range(1, nb_epochs):
     mini_batches_i = [list_indices_i[epoch][k:k+mini_batch_size] for k in range(0, N, mini_batch_size)]
     mini_batches_j = [list_indices_j[epoch][k:k+mini_batch_size] for k in range(0, N, mini_batch_size)]
     for m in range(mini_batch_size):
-        SAGAStep(mini_batches_i[m],mini_batches_j[m])
-    newLoglikelihood = LogLikelihood() + 1600
+        for _ in range(2):
+            SAGAStep(mini_batches_i[m],mini_batches_j[m])
+            MStep()
+    newLoglikelihood = LogLikelihood() 
     print("[", time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())), "] ", epoch+1, " epoch  ", str(newLoglikelihood))
     # if(oldLoglikelihood != 1 and newLoglikelihood - oldLoglikelihood < threshold):
     #     break
     objectiveSAGA.append(newLoglikelihood)
     oldLoglikelihood = newLoglikelihood
+
+
+# with open('losses/sagatest', 'wb') as fp: 
+#     pickle.dump(objectiveSAGA, fp)
